f3aa03c2d2883eb538208210ded0efefa3a84381
Jon Gray <jgray@spartaglobal.com>
2018-11-26 14:14:52 +0000

    first attempt at solving xml lab problems

---
+++ XMLLab/XMLLab/spec/xml_menu_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "XML menu" do
+
+  before(:all) do
+    @xml_menu = GuiseppesMenu.new
+  end
+
+  it "no price should be more than £10" do
+    @xml_menu.remove_first_char.each do |price|
+    expect(price).to be < 10
+    end
+  end
+
+  it "should have no item with calories over 1000 except for the full breakfast" do
+    # Consider children element methods - Remember to step through the data and print out to the command line if you need it
+    i=0
+    while i< @xml_menu.get_name.length
+      if i!= @xml_menu.find_index_name("Full Breakfast")
+        expect(@xml_menu.get_calories[i].text.to_i).to be < 1000
+      else
+        expect(@xml_menu.get_calories[i].text.to_i).to be > 1000
+      end
+      i += 1
+    end
+  end
+
+  it "should have all waffle dishes stating you get two waffles" do
+    # Consider children element methods - Remember to step through the data and print out to the command line if you need it
+    j=0
+    while j< @xml_menu.get_description.length
+      puts j
+      puts @xml_menu.get_description.length
+      if j == @xml_menu.find_index_name("Belgian Waffles")
+        expect(@xml_menu.get_description[j].text.upcase.include?("TWO")).to be true
+      end
+      j += 1
+    end
+  end
+
+end
